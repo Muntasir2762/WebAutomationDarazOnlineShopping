f62362fca8285cd74eb16abc0e5573193d8d5465
Muntasir-Pranto <fahimmuntasir43@gmail.com>
2021-12-01 13:37:09 +0600

    Product Add to cart and Checkout

--- WebAutomationDaraz/src/test/java/TestCases/TC0001.java
+++ WebAutomationDaraz/src/test/java/TestCases/TC0001.java
@@ -20,23 +20,111 @@ public class TC0001 extends DriverSetup {
 		driver.manage().window().maximize();
 		Thread.sleep(3000);
 		
-		driver.findElement(By.xpath("//a[contains(text(),'Signup / Login')]")).click();
-		Thread.sleep(3000);
+		//Title Validation....
+		String actualTitle="Online Shopping in Bangladesh: Order Now from Daraz.com.bd";
+		String getTitle=driver.getTitle();
+		System.out.println(getTitle);
 		
-		driver.findElement(By.xpath("//input[@placeholder='Please enter your Phone Number or Email']")).sendKeys("01624431977");
-		Thread.sleep(3000);
+		if(getTitle.equalsIgnoreCase(actualTitle)) {
+			System.out.println("Title Match");
+			
+			//Signup or Login Button Validation...
+			String login="Signup / Login";
+			String loginText= driver.findElement(By.xpath("//a[contains(text(),'Signup / Login')]")).getText();
+			
+			if(loginText.equalsIgnoreCase(login)) {
+				
+				driver.findElement(By.xpath("//a[contains(text(),'Signup / Login')]")).click();
+				Thread.sleep(3000);
+				
+				driver.findElement(By.xpath("//input[@placeholder='Please enter your Phone Number or Email']")).sendKeys("01624431977");
+				Thread.sleep(3000);
+				
+				driver.findElement(By.xpath("//input[@type='password']")).sendKeys("Muntasir1998");
+				Thread.sleep(3000);
+				
+				driver.findElement(By.xpath("//button[contains(text(),'LOGIN')]")).click();
+				Thread.sleep(3000);
+				
+				//Electronoc Devices Validation...
+				String EDevices="Electronic Devices";
+				String EDgetText=driver.findElement(By.xpath("//span[contains(text(),'Electronic Devices')]")).getText();
+				
+				if(EDgetText.equalsIgnoreCase(EDevices)) {
+					driver.findElement(By.xpath("//span[contains(text(),'Electronic Devices')]")).click();
+					Thread.sleep(3000);
+				}
+				
+				else {
+					driver.close();
+				}
+				
+				String DeviceCat="Smartphones";
+				String DeviceCatgetText=driver.findElement(By.xpath("//span[contains(text(),'Smartphones')]")).getText();
+				
+				if(DeviceCatgetText.equalsIgnoreCase(DeviceCat)) {
+					driver.findElement(By.xpath("//span[contains(text(),'Smartphones')]")).click();
+					Thread.sleep(3000);
+				}
+				
+				else {
+					driver.close();
+				}
+				
+				String PhoneName="Realme C21Y - 4GB RAM/64GB ROM";
+				String PhoneNamegetText=driver.findElement(By.xpath("//a[contains(text(),'Realme C21Y - 4GB RAM/64GB ROM')]")).getText();
+				
+				if(PhoneNamegetText.equalsIgnoreCase(PhoneName)) {
+					driver.findElement(By.xpath("//a[contains(text(),'Realme C21Y - 4GB RAM/64GB ROM')]")).click();
+					Thread.sleep(3000);
+				}
+				else {
+					System.out.println("Phone is note found!!");
+					driver.close();
+				}
+				
+				
+				//Add to Cart.....
+				driver.findElement(By.xpath("//span[contains(text(),'Add to Cart')]")).click();
+				Thread.sleep(3000);
+				
+				//Checkout Validation....
+				String CheckoutVd="CHECK OUT";
+				String CheckoutVdgetText=driver.findElement(By.xpath("//button[contains(text(),'CHECK OUT')]")).getText();
+				
+				if(CheckoutVdgetText.equalsIgnoreCase(CheckoutVd)) {
+					driver.findElement(By.xpath("//button[contains(text(),'CHECK OUT')]")).click();
+					Thread.sleep(3000);
+				}
+				
+				else {
+					System.out.println("Check out button is not found!!");
+					driver.close();
+				}
+				
+				
+				
+				//Logout Code.....
+				driver.findElement(By.xpath("//span[@id='myAccountTrigger']")).click();
+				Thread.sleep(3000);
+				
+				driver.findElement(By.xpath("//span[@class='account-icon test logout']")).click();
+				Thread.sleep(3000);
+			}
+			
+			else {
+				driver.close();
+			}
+			
+		}
 		
-		driver.findElement(By.xpath("//input[@type='password']")).sendKeys("Muntasir1998");
-		Thread.sleep(3000);
 		
-		driver.findElement(By.xpath("//button[contains(text(),'LOGIN')]")).click();
-		Thread.sleep(3000);
+		else {
+			System.out.println("Not Match");
+			driver.close();
+		}
 		
-		driver.findElement(By.xpath("//span[@id='myAccountTrigger']")).click();
-		Thread.sleep(3000);
 		
-		driver.findElement(By.xpath("//span[@class='account-icon test logout']")).click();
-		Thread.sleep(3000);
 		
 	}
 
