37f99f539a2815c3b43c8ce00e5e06fc66c07125
Muntasir-Pranto <fahimmuntasir43@gmail.com>
2021-12-04 23:22:03 +0600

    Validation With Assertion

--- WebAutomationDaraz/src/test/java/TestCases/TC0001.java
+++ WebAutomationDaraz/src/test/java/TestCases/TC0001.java
@@ -3,7 +3,10 @@ package TestCases;
 import java.time.Duration;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import BasePackage.DriverSetup;
@@ -20,8 +23,71 @@ public class TC0001 extends DriverSetup {
 		driver.manage().window().maximize();
 		Thread.sleep(3000);
 		
+		
+		//All validations with Assertion....
 		//Title Validation....
-		String actualTitle="Online Shopping in Bangladesh: Order Now from Daraz.com.bd";
+		String actualTitle = "Online Shopping in Bangladesh: Order Now from Daraz.com.bd";
+		String getTitle = driver.getTitle();
+		System.out.println(getTitle);
+		
+		Assert.assertEquals(actualTitle, getTitle);
+		
+		//Electronoc Devices Validation...
+		String EDevices="Electronic Devices";
+		String EDgetText=driver.findElement(By.xpath("//span[contains(text(),'Electronic Devices')]")).getText();
+		
+		Assert.assertEquals(EDevices, EDgetText);
+		driver.findElement(By.xpath("//span[contains(text(),'Electronic Devices')]")).click();
+		Thread.sleep(3000);
+		
+		WebElement hover=driver.findElement(By.xpath("//span[contains(text(),'Smartphones')]"));
+		
+		Actions action=new Actions(driver);
+		action.clickAndHold(hover).build().perform();
+		
+		driver.findElement(By.xpath("//span[contains(text(),'realme Phones')]")).click();
+		Thread.sleep(2000);
+		
+		//Specific Product Choose....
+		String PhoneName="Realme C21Y - 4GB RAM/64GB ROM";
+		String PhoneNamegetText=driver.findElement(By.xpath("//a[contains(text(),'Realme C21Y - 4GB RAM/64GB ROM')]")).getText();
+		
+		Assert.assertEquals(PhoneName, PhoneNamegetText);
+		driver.findElement(By.xpath("//a[contains(text(),'Realme C21Y - 4GB RAM/64GB ROM')]")).click();
+		Thread.sleep(3000);
+		
+		//Add to Cart....
+		driver.findElement(By.xpath("//span[contains(text(),'Add to Cart')]")).click();
+		Thread.sleep(3000);
+		
+		//iframe login....
+		WebElement loginiframe=driver.findElement(By.xpath("//iframe[@class='login-iframe']"));
+		driver.switchTo().frame(loginiframe);
+		
+		//login Credentials....
+		driver.findElement(By.xpath("//input[@placeholder='Please enter your Phone Number or Email']")).sendKeys("01624431977");
+		Thread.sleep(3000);
+		
+		driver.findElement(By.xpath("//input[@type='password']")).sendKeys("Muntasir1998");
+		Thread.sleep(3000);
+		
+		driver.findElement(By.xpath("//button[contains(text(),'LOGIN')]")).click();
+		Thread.sleep(3000);
+		
+		driver.switchTo().parentFrame();
+		
+		//Checkout Validation....
+		String CheckoutVd="CHECK OUT";
+		String CheckoutVdgetText=driver.findElement(By.xpath("//button[contains(text(),'CHECK OUT')]")).getText();
+		Assert.assertEquals(CheckoutVd, CheckoutVdgetText);
+		driver.findElement(By.xpath("//button[contains(text(),'CHECK OUT')]")).click();
+		Thread.sleep(3000);
+		
+		
+		
+		//All Validations With if/else condition....
+		//Title Validation....
+		/*String actualTitle="Online Shopping in Bangladesh: Order Now from Daraz.com.bd";
 		String getTitle=driver.getTitle();
 		System.out.println(getTitle);
 		
@@ -122,7 +188,7 @@ public class TC0001 extends DriverSetup {
 		else {
 			System.out.println("Not Match");
 			driver.close();
-		}
+		}*/
 		
 		
 		
